b1b209e5648cc3b0af7f4925c6485d8d86d348ae
khunderscorehh98 <haziqimac@gmail.com>
2024-11-25 11:42:24 +0800

    notification sections

--- src/components/news.vue
+++ src/components/news.vue
@@ -1,34 +1,56 @@
 <template>
     <div>
-        <v-card>
+        <v-card class="full-screen-card">
             <v-container>
                 <em class="grey-text">
                     News:
                 </em>
                 <hr>
                 <br>
-               <v-card>
-                <v-container>
-                    News Title
-                    <hr>
-                    <p>
-                        News Content
-                    </p>
-                </v-container>
-               </v-card>
+                <v-card>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12">
+                                News Title
+                            </v-col>
+                        </v-row>
+                        <hr>
+                        <v-row>
+                            <v-col>
+                                <p>
+                                    News Content
+                                </p>
+                            </v-col>
+                        </v-row>
+                        <v-row justify="center">
+                            <v-col cols="auto">
+                                <v-btn text>
+                                    READ MORE
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
             </v-container>
         </v-card>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style lang="scss" scoped>
 .grey-text {
     color: grey;
+}
+
+.full-screen-card {
+    height: 90vh;
+    /* Full viewport height */
+    width: 100vw;
+    /* Full viewport width */
 }
 </style>
--- src/components/notification.vue
+++ src/components/notification.vue
@@ -1,62 +1,55 @@
 <template>
-    <div>
-      <v-card class="full-screen-card d-flex flex-column">
-        <v-container>
-          <em class="grey-text">
-            Notifications:
-          </em>
-          <hr>
-          <br>
-          <v-card>
-            <v-container>
-                <v-row>
-                    <v-col>
+  <div>
+    <v-card class="full-screen-card d-flex flex-column">
+      <v-container>
+        <em class="grey-text">
+          Notifications:
+        </em>
+        <hr>
+        <br>
+        <v-card>
+          <v-container>
+            <v-row>
+              <v-col>
+                Notification sample
+              </v-col>
+            </v-row>
+            <hr>
+            <v-row>
+              <v-col>
+                Notification Description
+              </v-col>
+            </v-row>
+            <v-row justify="center">
+              <v-col cols="auto">
+                <v-btn
+                  text
+                >
+                  READ MORE
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-container>
+    </v-card>
+  </div>
+</template>
 
-                        Notification sample
-                    </v-col>
-                </v-row>
-                <hr>
-                <v-row>
-                    <v-col>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
 
-                        Notification Description
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="9"></v-col>
-                    <v-col cols="3">
-                        <v-btn
-                        icon
-                        x-large
-                        >
-                            <v-icon>
-                                mdi-cursor-default-click-outline
-                            </v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-          </v-card>
-        </v-container>
-      </v-card>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {};
-    },
-  };
-  </script>
-  
-  <style lang="scss" scoped>
-  .full-screen-card {
-    height: 100vh; /* Full viewport height */
-    width: 100vw; /* Full viewport width */
-  }
-  .grey-text {
-    color: grey;
-  }
-  </style>
-  
+<style lang="scss" scoped>
+.full-screen-card {
+  height: 90vh; /* Full viewport height */
+  width: 100vw; /* Full viewport width */
+}
+.grey-text {
+  color: grey;
+}
+</style>
